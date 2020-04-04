bdbdad5650cefc84ff96aef58257247779ab5dba
Austin Ricketts <austinricketts4@gmail.com>
2020-04-04 16:46:01 -0400

    ajax call workingin console.log(), new card added for five-day forecast

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-/*$(document).ready(function() {
+$(document).ready(function() {
+    
+    $(".btn-primary").on("click", function() {
+        var apiKey = "14275cb63b6d277176cc8032394e4196";
+        var userInput = $.trim($("#city-search").val().toString());
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            .then(function(response) {
+              console.log(response);
+        
+        
+              
+              
+            });
+      });
+
+    /*getStorage();
+
+    function getStorage() {
+        var cityNames = JSON.parse(localStorage.getItem("cities", ""));
+        console.log(cityNames);
+    
+        $("#city-names").val(cityNames); 
+    }
+
+    $(".btn-primary").on("click", function(event) {
+        event.preventDefault();
+        var cityInfo = $.trim($("#city-search").val());
+        localStorage.setItem("cities", JSON.stringify(cityInfo));
+        console.log(cityInfo);
+        $("#city-names").prepend(cityInfo);
+      });*/
+
+      
+
+
+
+    
 
         
         
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-      
-      console.log(response.data);
-
-
-      
-      
-    });
+   
   
 
 
@@ -25,4 +55,4 @@
 
 
 
-});*/
+});
